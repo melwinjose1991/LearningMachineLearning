7f9796e8f41d6b92f7db7ad66b003dab3c25bb9c
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-03-18 15:34:38 -0400

    CNN :: Pretty

---
+++ python/learning - tensor-flow/CNNs/CNN_Pretty.py
@@ -0,0 +1,123 @@
+## source: https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/03_PrettyTensor.ipynb
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import prettytensor as pt
+
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets('data/MNIST/', one_hot=True)
+data.test.cls = np.argmax(data.test.labels, axis=1)
+
+img_size = 28
+img_size_flat = img_size * img_size
+img_shape = (img_size, img_size)
+num_channels = 1
+num_classes = 10
+
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+    
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        ax.set_xlabel(xlabel)
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    plt.show()
+    
+
+x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='x')
+x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+
+## Pretty
+x_pretty = pt.wrap(x_image)
+with pt.defaults_scope(activation_fn=tf.nn.relu):
+    y_pred, loss = x_pretty.\
+        conv2d(kernel=5, depth=16, name='layer_conv1').\
+        max_pool(kernel=2, stride=2).\
+        conv2d(kernel=5, depth=36, name='layer_conv2').\
+        max_pool(kernel=2, stride=2).\
+        flatten().\
+        fully_connected(size=128, name='layer_fc1').\
+        softmax_classifier(num_classes=num_classes, labels=y_true)
+   
+        
+def get_weights_variable(layer_name):
+    with tf.variable_scope(layer_name, reuse=True):
+        variable = tf.get_variable('weights')
+    return variable
+
+
+weights_conv1 = get_weights_variable(layer_name='layer_conv1')
+weights_conv2 = get_weights_variable(layer_name='layer_conv2')
+
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+
+train_batch_size = 64
+total_iterations = 0
+def optimize(num_iterations):
+    global total_iterations
+
+    for i in range(total_iterations,
+                   total_iterations + num_iterations):
+
+        x_batch, y_true_batch = data.train.next_batch(train_batch_size)
+        feed_dict_train = {x: x_batch, y_true: y_true_batch}
+
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+        if i % 100 == 0:
+            acc = session.run(accuracy, feed_dict=feed_dict_train)
+            msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
+            print(msg.format(i + 1, acc))
+
+    total_iterations += num_iterations
+
+
+test_batch_size = 256
+def print_test_accuracy():
+    num_test = len(data.test.images)
+    cls_pred = np.zeros(shape=num_test, dtype=np.int)
+
+    i = 0
+
+    while i < num_test:
+        j = min(i + test_batch_size, num_test)
+        images = data.test.images[i:j, :]
+        labels = data.test.labels[i:j, :]
+        feed_dict = {x: images,
+                     y_true: labels}
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+        i = j
+
+    cls_true = data.test.cls
+    correct = (cls_true == cls_pred)
+    correct_sum = correct.sum()
+    acc = float(correct_sum) / num_test
+    msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+    print(msg.format(acc, correct_sum, num_test))
+
+
+optimize(num_iterations=1)
+print_test_accuracy()
