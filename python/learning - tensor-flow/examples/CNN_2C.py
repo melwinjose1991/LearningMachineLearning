ce9a403ead130269668375c25315370cfee7aacb
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-03-18 01:16:39 -0400

    CNN

---
+++ python/learning - tensor-flow/examples/CNN_2C.py
@@ -0,0 +1,384 @@
+# source : https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/02_Convolutional_Neural_Network.ipynb
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import time
+from datetime import timedelta
+import math
+from tensorflow.examples.tutorials.mnist import input_data
+
+data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+data.test.cls = np.argmax(data.test.labels, axis=1)
+
+img_size = 28
+img_size_flat = img_size * img_size
+img_shape = (img_size, img_size)
+num_channels = 1  # Number of colour channels for the images: 1 channel for gray-scale.
+num_classes = 10
+
+# Convolutional Layer 1.
+filter_size1 = 5  # Convolution filters are 5 x 5 pixels.
+num_filters1 = 16  # There are 16 of these filters.
+
+# Convolutional Layer 2.
+filter_size2 = 5          # Convolution filters are 5 x 5 pixels.
+num_filters2 = 36         # There are 36 of these filters.
+
+# Fully-connected layer.
+fc_size = 128  # Number of neurons in fully-connected layer.
+
+def new_conv_layer(input,               # The previous layer.
+                   num_input_channels,  # Num. channels in prev. layer.       
+                   filter_size,         # Width and height of each filter.    
+                   num_filters,         # Number of filters.                  
+                   use_pooling=True):   # Use 2x2 max-pooling.
+
+    # Shape of the filter-weights for the convolution.
+    # This format is determined by the TensorFlow API.
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+    # [5, 5, 1, 16]
+    # [5, 5, 16, 36]
+
+    # Create new weights aka. filters with the given shape.
+    weights = tf.Variable(tf.truncated_normal(shape, stddev=0.001))
+    # [5, 5, 1, 16]
+    # [5, 5, 16, 36]
+
+    # Create new biases, one for each filter.
+    biases = tf.Variable(tf.constant(0.05, shape=[num_filters]))
+
+    # Create the TensorFlow operation for convolution.
+    # Note the strides are set to 1 in all dimensions.
+    # The first and last stride must always be 1,
+    # because the first is for the image-number and
+    # the last is for the input-channel.
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image.
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroes so the size of the output is the same.
+    layer = tf.nn.conv2d(input=input,               # x_image [-1. 28, 28, 1]
+                         filter=weights,            # weights [5, 5, 1, 16]
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+    # [ ?, 28, 28, 16]
+    # [ ?, 14, 14, 36]
+
+    # Add the biases to the results of the convolution.
+    # A bias-value is added to each filter-channel.
+    layer += biases
+
+    # Use pooling to down-sample the image resolution?
+    if use_pooling:
+        # This is 2x2 max-pooling, which means that we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+    # [ ?, 14, 14, 16]
+    # [ ?,  7,  7, 36]
+
+    # Rectified Linear Unit (ReLU).
+    # It calculates max(x, 0) for each input pixel x.
+    # This adds some non-linearity to the formula and allows us
+    # to learn more complicated functions.
+    layer = tf.nn.relu(layer)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the filter-weights
+    # because we will plot the weights later.
+    return layer, weights
+
+
+def flatten_layer(layer):
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+    layer_shape = layer.get_shape()
+    # [ ?, 7, 7, 36]
+
+    # The number of features is: img_height * img_width * num_channels
+    num_features = layer_shape[1:4].num_elements()
+    # 1764
+    
+    # [num_images, img_height * img_width * num_channels]
+    layer_flat = tf.reshape(layer, [-1, num_features])
+    # [ ?, 1764 ]
+
+    return layer_flat, num_features
+
+
+def new_fully_connected_layer(input,    # The previous layer.
+                 num_inputs,            # Num. inputs from prev. layer.
+                 num_outputs,           # Num. outputs.
+                 use_relu=True):        # Use Rectified Linear Unit (ReLU)?
+
+    weights = tf.Variable(tf.truncated_normal([num_inputs, num_outputs], stddev=0.05))
+    # [1764, 128]
+    # [128, 10]
+    
+    biases = tf.Variable(tf.constant(0.05, shape=[num_outputs]))
+    # [128]
+    # [10]
+    
+    layer = tf.matmul(input, weights) + biases
+    # [?, 128]
+    # [?, 10]
+
+    # Use ReLU?
+    if use_relu:
+        layer = tf.nn.relu(layer)
+
+    return layer
+
+
+x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='x')
+x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+
+y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+layer_conv1, weights_conv1 = \
+    new_conv_layer(input=x_image,                       # [ ?, 28, 28, 1]
+                   num_input_channels=num_channels,     # 1
+                   filter_size=filter_size1,            # 5
+                   num_filters=num_filters1,            # 16
+                   use_pooling=True)
+# [ ?, 14, 14, 16]
+
+layer_conv2, weights_conv2 = \
+    new_conv_layer(input=layer_conv1,                   # [?, 14, 14, 16]
+                   num_input_channels=num_filters1,     # 16
+                   filter_size=filter_size2,            # 5
+                   num_filters=num_filters2,            # 36
+                   use_pooling=True)
+# [ ?, 7, 7, 36]
+    
+layer_flat, num_features = flatten_layer(layer_conv2)
+# [?, 1764], 1764
+
+layer_fc1 = new_fully_connected_layer(input=layer_flat, # output of CNL1 [?, 1764]
+                         num_inputs=num_features,       # 1764
+                         num_outputs=fc_size,           # 128
+                         use_relu=True)
+# [?, 128]
+
+layer_fc2 = new_fully_connected_layer(input=layer_fc1,  # output of FC1 [?, 128]
+                         num_inputs=fc_size,            # 128
+                         num_outputs=num_classes,       # 10
+                         use_relu=False)
+# [?, 10]
+
+y_pred = tf.nn.softmax(layer_fc2)
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2, labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+train_batch_size = 64
+total_iterations = 0
+
+def optimize(num_iterations):
+    # Ensure we update the global variable rather than a local copy.
+    global total_iterations
+
+    for i in range(total_iterations,
+                   total_iterations + num_iterations):
+
+        # x_batch = batch of images
+        # y_true_batch are the true labels for those images.
+        x_batch, y_true_batch = data.train.next_batch(train_batch_size)
+
+        # Put the batch into a dict with the proper names
+        # for placeholder variables in the TensorFlow graph.
+        feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+
+        # Run the optimizer using this batch of training data.
+        # TensorFlow assigns the variables in feed_dict_train
+        # to the placeholder variables and then runs the optimizer.
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+        # Print status every 100 iterations.
+        if i % 100 == 0:
+            # Calculate the accuracy on the training-set.
+            acc = session.run(accuracy, feed_dict=feed_dict_train)
+
+            # Message for printing.
+            msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
+
+            # Print it.
+            print(msg.format(i + 1, acc))
+
+    # Update the total number of iterations performed.
+    total_iterations += num_iterations
+
+
+test_batch_size = 256
+def print_test_accuracy(show_example_errors=False,
+                        show_confusion_matrix=False):
+
+    # Number of images in the test-set.
+    num_test = len(data.test.images)
+
+    # Allocate an array for the predicted classes which
+    # will be calculated in batches and filled into this array.
+    cls_pred = np.zeros(shape=num_test, dtype=np.int)
+
+    # Now calculate the predicted classes for the batches.
+    # We will just iterate through all the batches.
+    # There might be a more clever and Pythonic way of doing this.
+
+    # The starting index for the next batch is denoted i.
+    i = 0
+
+    while i < num_test:
+        # The ending index for the next batch is denoted j.
+        j = min(i + test_batch_size, num_test)
+
+        # Get the images from the test-set between index i and j.
+        images = data.test.images[i:j, :]
+
+        # Get the associated labels.
+        labels = data.test.labels[i:j, :]
+
+        # Create a feed-dict with these images and labels.
+        feed_dict = {x: images,
+                     y_true: labels}
+
+        # Calculate the predicted class using TensorFlow.
+        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+        # Set the start-index for the next batch to the
+        # end-index of the current batch.
+        i = j
+
+    # Convenience variable for the true class-numbers of the test-set.
+    cls_true = data.test.cls
+
+    # Create a boolean array whether each image is correctly classified.
+    correct = (cls_true == cls_pred)
+
+    # Calculate the number of correctly classified images.
+    # When summing a boolean array, False means 0 and True means 1.
+    correct_sum = correct.sum()
+
+    # Classification accuracy is the number of correctly classified
+    # images divided by the total number of images in the test-set.
+    acc = float(correct_sum) / num_test
+
+    # Print the accuracy.
+    msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+    print(msg.format(acc, correct_sum, num_test))
+
+
+def plot_conv_weights(weights, input_channel=0):
+    # Assume weights are TensorFlow ops for 4-dim variables
+    # e.g. weights_conv1 or weights_conv2.
+    
+    # Retrieve the values of the weight-variables from TensorFlow.
+    # A feed-dict is not necessary because nothing is calculated.
+    w = session.run(weights)
+
+    # Get the lowest and highest values for the weights.
+    # This is used to correct the colour intensity across
+    # the images so they can be compared with each other.
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Number of filters used in the conv. layer.
+    num_filters = w.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+    
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot all the filter-weights.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the valid filter-weights.
+        if i < num_filters:
+            # Get the weights for the i'th filter of the input channel.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = w[:, :, input_channel, i]
+
+            # Plot image.
+            ax.imshow(img, vmin=w_min, vmax=w_max,
+                      interpolation='nearest', cmap='seismic')
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+    
+    
+def plot_conv_layer(layer, image):
+    # Assume layer is a TensorFlow op that outputs a 4-dim tensor
+    # which is the output of a convolutional layer,
+    # e.g. layer_conv1 or layer_conv2.
+
+    # Create a feed-dict containing just one image.
+    # Note that we don't need to feed y_true because it is
+    # not used in this calculation.
+    feed_dict = {x: [image]}
+
+    # Calculate and retrieve the output values of the layer
+    # when inputting that image.
+    values = session.run(layer, feed_dict=feed_dict)
+
+    # Number of filters used in the conv. layer.
+    num_filters = values.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+    
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot the output images of all the filters.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the images for valid filters.
+        if i < num_filters:
+            # Get the output image of using the i'th filter.
+            # See new_conv_layer() for details on the format
+            # of this 4-dim tensor.
+            img = values[0, :, :, i]
+
+            # Plot image.
+            ax.imshow(img, interpolation='nearest', cmap='binary')
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+optimize(num_iterations=10000)
+print_test_accuracy()
+
+# plotting weights and output
+image = data.test.images[1]
+plot_conv_weights(weights=weights_conv1)
+plot_conv_layer(layer=layer_conv1, image=image)
+
+plot_conv_weights(weights=weights_conv2, input_channel=0)
+plot_conv_layer(layer=layer_conv2, image=image)
