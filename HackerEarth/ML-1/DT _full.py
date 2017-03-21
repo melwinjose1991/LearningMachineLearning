e352c257d88c73942a2622cc43253dff5e6b4372
melwin jose <melwin.jose.1991@gmail.com>
2017-03-21 19:28:04 -0400

    Re-Org

--- HackerEarth/ML-1/DT _full.py
+++ HackerEarth/ML-1/DT _full.py
@@ -35,8 +35,6 @@ data_star = data[cat_columns].copy()
 data_star[cat_columns] = data_star[cat_columns].apply(lambda x: x.cat.codes)
 
 
-
-### Numerical Predictors
 ## annual_inc
 data_star['annual_inc']=data['annual_inc']
 inc_nan_rows =  data_star['annual_inc'].index[data_star['annual_inc'].apply(np.isnan)]
@@ -49,60 +47,37 @@ data_star['loan_amnt']=data['loan_amnt']
 ## funded_amnt
 data_star['funded_amnt']=data['funded_amnt']
 
+## pub_rec
+pub_rec_nan_rows =  data['pub_rec'].index[data['pub_rec'].apply(np.isnan)]
+data_star['pub_rec'] = data.set_value(pub_rec_nan_rows, col='pub_rec', value=0)['pub_rec']
+
 ## funded_amnt_inv
 data_star['funded_amnt_inv']=data['funded_amnt_inv']
-
-## delinq_2yrs
-var = 'delinq_2yrs'
-nan_rows =  data[var].index[data[var].apply(np.isnan)]
-data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-
-## inq_last_6mths
-var = 'inq_last_6mths'
-nan_rows =  data[var].index[data[var].apply(np.isnan)]
-data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-
-## total_rec_late_fee
-var = 'total_rec_late_fee'
-nan_rows =  data[var].index[data[var].apply(np.isnan)]
-data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-
-## tot_cur_bal
-var = 'tot_cur_bal'
-nan_rows =  data[var].index[data[var].apply(np.isnan)]
-data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-
-## pub_rec
-var = 'pub_rec'
-nan_rows =  data[var].index[data[var].apply(np.isnan)]
-data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-
 
 ## Predictors 
 predictors = ['term', 'batch_enrolled', 'grade', 'sub_grade', 'emp_length', 'home_ownership', 'verification_status', \
               'pymnt_plan', 'purpose', 'zip_code', 'addr_state', 'application_type', 'initial_list_status', 'verification_status_joint', \
               'verification_status_joint', 'last_week_pay', \
-              'annual_inc', 'loan_amnt', 'funded_amnt', 'funded_amnt_inv', 'delinq_2yrs', 'inq_last_6mths', 'total_rec_late_fee', 'tot_cur_bal', 'pub_rec']
+              'annual_inc', 'loan_amnt', 'funded_amnt', 'funded_amnt_inv', 'pub_rec']
 
+## Random Forest
+train, test = train_test_split(data_star, test_size = 0.25)
 
-## Decision Trees
-for i in range(0,5):
-    train, test = train_test_split(data_star, test_size = 0.25)
-    
-    #print("Training on train")
-    rf = tree.DecisionTreeClassifier(min_samples_split=0.02, criterion="entropy")
-    rf_fit = rf.fit(train[predictors], train[['loan_status']].values.ravel())
-    
-    #with open("tree.dot", 'w') as f:
-    #    f = tree.export_graphviz(rf_fit, out_file=f)
-    
-    #print("Predicting test")
-    Y_pred = rf_fit.predict(test[predictors])
-    Y_actual = [test['loan_status']]
-    total = len((Y_actual == Y_pred)[0])
-    correct = np.sum(Y_actual == Y_pred)
-    
-    print("accuracy:",float(correct)/total)
+print("Training on train")
+#rf = RandomForestClassifier(n_estimators=100, n_jobs=4)
+rf = tree.DecisionTreeClassifier(min_samples_split=0.02)
+rf_fit = rf.fit(train[predictors], train[['loan_status']].values.ravel())
+
+with open("tree.dot", 'w') as f:
+    f = tree.export_graphviz(rf_fit, out_file=f)
+
+print("Predicting test")
+Y_pred = rf_fit.predict(test[predictors])
+Y_actual = [test['loan_status']]
+total = len((Y_actual == Y_pred)[0])
+correct = np.sum(Y_actual == Y_pred)
+
+print("accuracy:",float(correct)/total)
 
 
 ## Test Data
@@ -131,46 +106,17 @@ if True:
     data_star = data[cat_columns].copy()
     data_star[cat_columns] = data_star[cat_columns].apply(lambda x: x.cat.codes)
     
-    ### Numerical Predictors
-    ## annual_inc
     data_star['annual_inc']=data['annual_inc']
     inc_nan_rows =  data_star['annual_inc'].index[data_star['annual_inc'].apply(np.isnan)]
     inc_mean = np.mean(data_star['annual_inc'])
     data_star.loc[inc_nan_rows,'annual_inc'] = inc_mean
     
-    ## loan_amnt
     data_star['loan_amnt']=data['loan_amnt']
-    
-    ## funded_amnt
     data_star['funded_amnt']=data['funded_amnt']
-    
-    ## funded_amnt_inv
     data_star['funded_amnt_inv']=data['funded_amnt_inv']
     
-    ## delinq_2yrs
-    var = 'delinq_2yrs'
-    nan_rows =  data[var].index[data[var].apply(np.isnan)]
-    data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-    
-    ## inq_last_6mths
-    var = 'inq_last_6mths'
-    nan_rows =  data[var].index[data[var].apply(np.isnan)]
-    data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-    
-    ## total_rec_late_fee
-    var = 'total_rec_late_fee'
-    nan_rows =  data[var].index[data[var].apply(np.isnan)]
-    data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-    
-    ## tot_cur_bal
-    var = 'tot_cur_bal'
-    nan_rows =  data[var].index[data[var].apply(np.isnan)]
-    data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
-    
-    ## pub_rec
-    var = 'pub_rec'
-    nan_rows =  data[var].index[data[var].apply(np.isnan)]
-    data_star[var] = data.set_value(nan_rows, col=var, value=0)[var]
+    pub_rec_nan_rows =  data['pub_rec'].index[data['pub_rec'].apply(np.isnan)]
+    data_star['pub_rec'] = data.set_value(pub_rec_nan_rows, col='pub_rec', value=0)['pub_rec']
 
  
     Y_pred = rf_fit.predict_proba(data_star[predictors])
