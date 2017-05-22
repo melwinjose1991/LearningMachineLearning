adbf5145348016e3833017be4296e88e7f655f78
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-05-21 21:35:03 -0400

    ISLR :: regression :: excersie

--- R/ISLR/3 - Linear Regression/Ex8.R
+++ R/ISLR/3 - Linear Regression/Ex8.R
@@ -1,7 +1,10 @@
 library(ISLR)
 library(car)
+library(MASS)
 
 data(Auto)
+
+
 
 # 8
   # a
@@ -22,6 +25,7 @@ data(Auto)
   # non-linear relationship
   
 
+  
 # 9
   #a
   pairs(Auto)
@@ -90,4 +94,74 @@ data(Auto)
   summary(fit_all)
   # 0.888
   
+  
+  
+#10
+  #a-b
+  names(Carseats)
+  fit_1 = lm(Sales~Price+Urban+US, data=Carseats)
+  summary(fit_1)
+  
+  #e 
+  fit_2 = lm(Sales~Price+US, data=Carseats)
+  summary(fit_2)
+  
+  #f
+  #  Based on the RSE and R^2 of the linear regressions, they both fit 
+  #  the data similarly, with linear regression from (e) fitting the data slightly better
+  
+  #g
+  confint(fit_2)
+  
+  #h
+  plot(fit_2)
+  # (p+1)/n = 0.0075, lots of points beyond that, they are 
+  # high leverage points
+  
+  plot(predict(fit_2), rstudent(fit_2))
+  # everything is within the -3,+3 interval, NO outliers
+  
+
+  
+#11
+  
+  #
+  set.seed(1)
+  x = rnorm(100)
+  y = 2*x + rnorm(100)
+
+  lm.fit = lm(y~x+0)
+  summary(lm.fit)  
+  
+  
+  
+# 13
+  set.seed(1)
+  x = rnorm(100)
+  eps = rnorm(100, 0, sqrt(0.25))
+  y = -1 + 0.5*x + eps
+  plot(x, y)
+  
+  lm.fit = lm(y~x)
+  summary(lm.fit)
+  
+  #f
+  par(mfrow=c(1,1))
+  plot(x, y)
+  abline(lm.fit, lwd=3, col=2)
+  abline(-1, 0.5, lwd=3, col=3)
+  legend(-1, legend = c("model fit", "pop. regression"), col=2:3, lwd=3)  
+  
+  #g
+  lm.fit = lm(y~x+I(x^2))
+  summary(lm.fit)
+  # R^2 and RSE indicates it is a better model
+  # but p-value of I(X^2) says it is insignificant
+
+    
+
+#15
+  names(Boston)
+  fit = lm(crim~., data=Boston)  
+  summary(fit)  
   
