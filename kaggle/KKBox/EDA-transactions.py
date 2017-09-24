637c689e2626d63c1704f6746499e4197e486608
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-09-24 10:15:00 -0400

    KKBox :: pivot table for payment_method_id

--- kaggle/KKBox/EDA-transactions.py
+++ kaggle/KKBox/EDA-transactions.py
@@ -1,15 +1,17 @@
-### Exploratory Data Analysis
+### Exploratory Data Analysis - train & transactions
 
 
 
 #%% Imports
 import pandas as pd
+import numpy as np
 import os
+from ggplot import *
 
 
 
-#%% reading the csv files
-os.chdir("C:\\Users\\USMEJOS\\workspace\\personal\\kaggle\\KKBox")
+#%% Reading the csv files
+os.chdir("C:\\Users\\Melwin Jose\\workspace\\LearningMachineLearning\\kaggle\\KKBox")
 
 eda_train_data = pd.read_csv("data/train.csv")
 eda_transactions_data = pd.read_csv("data/transactions.csv")
@@ -22,4 +24,12 @@ eda_transactions_data.dtypes
 
 
 
-#%%
+#%% payment_method_id
+tmp = eda_transactions_data.ix[:,0:2].copy()
+print("# of nulls in payment_method_id : ", pd.isnull(tmp.payment_method_id).sum())
+print("Unique values for payment_method_id : ", tmp.payment_method_id.unique().size)
+
+merged_df = pd.merge(eda_train_data, tmp, on='msno', how='left')
+pt = pd.pivot_table(merged_df, index=["payment_method_id"], values=["is_churn"], aggfunc=[len, np.sum, np.mean])
+pt = pd.DataFrame(pt.to_records())
+
